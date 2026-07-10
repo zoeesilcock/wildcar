ef920cb73755229cf65002a4447010a135fc7e5f
Zoee Silcock <mrzoee@gmail.com>
2026-07-10 14:26:27 +0200

    Remove accidentally committed zig-pkg

--- zig-pkg/flint-0.11.0-PsSSI33tAQBfA-UTkoIa_Fb9UBdS5uA5Y3jRPkG8IjA7/src/lib/imgui.h
+++
@@ -1,2 +0,0 @@
-#define IMGUI_USE_WCHAR32 1
-#include <dcimgui.h>
--- zig-pkg/flint-0.11.0-PsSSI33tAQBfA-UTkoIa_Fb9UBdS5uA5Y3jRPkG8IjA7/src/lib/sdl.h
+++
@@ -1,5 +0,0 @@
-#define SDL_DISABLE_OLD_NAMES
-#include <SDL3/SDL.h>
-#include <SDL3/SDL_revision.h>
-#define SDL_MAIN_HANDLED
-#include <SDL3/SDL_main.h>
