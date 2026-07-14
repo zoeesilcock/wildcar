437be5905bbd3801c736a337219df2e5bccfd51d
Zoee Silcock <mrzoee@gmail.com>
2026-07-14 08:11:19 +0200

    Move the scene code into its own file

--- src/root.zig
+++ src/root.zig
@@ -7,6 +7,7 @@ const imgui = flint.imgui;
 const math = @import("math");
 const debug_ui = if (INTERNAL) @import("debug_ui.zig") else undefined;
 const renderer = @import("render/renderer.zig");
+const scene = @import("scene.zig");
 
 const INTERNAL: bool = @import("build_options").internal;
 
@@ -121,39 +122,6 @@ const Entity = struct {
     is_dynamic: bool = false,
 };
 
-const SceneEntity = struct {
-    position: Vector3,
-    scale: Vector3,
-    rotation: Vector3,
-    color: Color = .{ 0.9, 0.3, 0.2, 1 },
-    is_dynamic: bool = false,
-};
-
-const Scene = struct {
-    items: []SceneEntity = &.{},
-
-    pub fn loadFromFile(path: []const u8, allocator: std.mem.Allocator, io: std.Io) Scene {
-        var scene: Scene = .{};
-        if (flint.fs.getFilePathRelative(io, path, allocator)) |relative_path| {
-            defer allocator.free(relative_path);
-
-            const scene_file = flint.fs.openFileRelative(io, relative_path, .{ .mode = .read_only }) catch
-                @panic("Failed to open scene file");
-            defer scene_file.close(io);
-
-            const scene_slice = std.Io.Dir.cwd().readFileAllocOptions(io, path, allocator, .unlimited, .@"1", 0) catch
-                @panic("Failed to read scene file");
-            defer allocator.free(scene_slice);
-
-            scene = std.zon.parse.fromSliceAlloc(Scene, allocator, scene_slice, null, .{}) catch
-                @panic("Failed to parse scene .zon file");
-        } else |_| {
-            @panic("Failed to open scene file");
-        }
-        return scene;
-    }
-};
-
 pub var settings: GameLib.Settings = .{
     .title = "Wildcar",
     .frame_rate = .fixed(60),
@@ -166,48 +134,6 @@ pub export fn getSettings() GameLib.Settings {
 fn getAspectRatio() f32 {
     return @as(f32, @floatFromInt(settings.window_width)) /
         @as(f32, @floatFromInt(settings.window_height));
-}
-
-fn loadScene(state: *State) void {
-    const scene: Scene = .loadFromFile("assets/scene.zon", state.allocator, state.dependencies.io.*);
-    defer std.zon.parse.free(state.allocator, scene);
-
-    for (scene.items) |item| {
-        const entity = state.entities.addOne(state.allocator) catch @panic("Failed to add entity");
-        entity.* = .{
-            .transform = .{
-                .position = item.position,
-                .scale = item.scale,
-                .rotation = math.eulerToQuaternion(item.rotation),
-            },
-            .is_dynamic = item.is_dynamic,
-            .color = item.color,
-        };
-
-        var body_def: c.b3BodyDef = c.b3DefaultBodyDef();
-        body_def.position = .{
-            .x = entity.transform.position[X],
-            .y = entity.transform.position[Y],
-            .z = entity.transform.position[Z],
-        };
-        body_def.type = if (entity.is_dynamic) c.b3_dynamicBody else c.b3_staticBody;
-
-        entity.body_id = c.b3CreateBody(state.world_id, &body_def);
-        const box: c.b3BoxHull = c.b3MakeBoxHull(
-            entity.transform.scale[X],
-            entity.transform.scale[Y],
-            entity.transform.scale[Z],
-        );
-
-        var shape_def: c.b3ShapeDef = c.b3DefaultShapeDef();
-        shape_def.density = 1;
-        shape_def.baseMaterial.friction = 0.3;
-        _ = c.b3CreateHullShape(entity.body_id, &shape_def, &box.base);
-    }
-}
-
-fn unloadScene(state: *State) void {
-    state.entities.clearRetainingCapacity();
 }
 
 pub export fn initFull3D(dependencies: GameLib.Dependencies.Full3D) GameLib.GameStatePtr {
@@ -250,7 +176,7 @@ pub export fn initFull3D(dependencies: GameLib.Dependencies.Full3D) GameLib.Game
     const ground_shape_def: c.b3ShapeDef = c.b3DefaultShapeDef();
     _ = c.b3CreateHullShape(ground_id, &ground_shape_def, &ground_box.base);
 
-    loadScene(state);
+    scene.load(state);
 
     return state;
 }
@@ -259,13 +185,13 @@ pub export fn deinit(state_ptr: GameLib.GameStatePtr) void {
     const state: *State = @ptrCast(@alignCast(state_ptr));
     renderer.deinit(&state.renderer);
     c.b3DestroyWorld(state.world_id);
-    unloadScene(state);
+    scene.unload(state);
 }
 
 pub export fn willReload(state_ptr: GameLib.GameStatePtr) void {
     const state: *State = @ptrCast(@alignCast(state_ptr));
     renderer.deinit(&state.renderer);
-    unloadScene(state);
+    scene.unload(state);
 }
 
 pub export fn reloaded(state_ptr: GameLib.GameStatePtr, imgui_context: ?*imgui.ImGuiContext) void {
@@ -287,7 +213,7 @@ pub export fn reloaded(state_ptr: GameLib.GameStatePtr, imgui_context: ?*imgui.I
     // Use this when working on camera.
     // state.camera = .init(getAspectRatio());
 
-    loadScene(state);
+    scene.load(state);
 }
 
 pub export fn processInput(state_ptr: GameLib.GameStatePtr) bool {
